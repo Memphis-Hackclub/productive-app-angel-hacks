6be004ae87e4ce4cb71a8ea89c292274b9d4d815
Philip Pounds <philipstudentemail@gmail.com>
2021-02-27 13:18:27 -0600

    changed these

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     let Adreess = tabs[0].url;
-    document.getElementById("submit").innerHTML = Adreess;
+    
     var link = String(tabs[0].url);
     document.getElementById("test").innerHTML = link;
     if (link=="https://www.youtube.com/"){
