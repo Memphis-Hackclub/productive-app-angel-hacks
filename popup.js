ecc27d2bd48bc188961654cd8c6b3b852a12a0a0
Philip Pounds <philipstudentemail@gmail.com>
2021-02-27 17:24:30 -0600

    In this commit we added a form and saving system!

--- popup.js
+++ popup.js
@@ -2,7 +2,8 @@
 localStorage.website0 = website0;
 document.getElementById("test3").innerHTML = localStorage.website0;*/
 
-
+excute = document.getElementById("excute");
+excute.addEventListener('click', getData);
 
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     let Adreess = tabs[0].url;
@@ -30,10 +31,40 @@ function myTimer() {
     var date = new Date();
     newtime = date.getTime();
     newtime = newtime/1000;
-    document.getElementById("test3").innerHTML = newtime;
     realtime = newtime-oldtime;
-    document.getElementById("test2").innerHTML = realtime;
+    document.getElementById("test2").innerHTML = "You have been here for " + realtime;
     return newtime
     
     }
 
+
+    function getData() {
+        //info for first website
+        time1 = document.getElementById("time").value;
+        website1 = document.getElementById("website").value;
+        level1 = document.getElementById("type").value;
+        alert(website1);
+        localStorage.setItem("time1", time);
+        localStorage.setItem("website1", website1);
+        localStorage.setItem("level1", level1);
+      
+        //info for second website
+        time2 = document.getElementById("time2").value;
+        website2 = document.getElementById("website2").value;
+        level2 = document.getElementById("type2").value;
+        localStorage.setItem("time2", time2);
+        localStorage.setItem("website2", website2);
+        localStorage.setItem("level2", level2);
+        
+        //info for third website
+        time3 = document.getElementById("time3").value;
+        website3 = document.getElementById("website3").value;
+        level3 = document.getElementById("type3").value;
+        localStorage.setItem("time3", time3);
+        localStorage.setItem("website3", website3);
+        localStorage.setItem("level3", level3);
+        
+        var websiteDummy = localStorage.getItem("website1");
+        document.getElementById("test").innerHTML = websiteDummy;
+      
+      }
