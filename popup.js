af913649cf1c2de14358fd2f9d63b6606cf5e5cb
Philip Pounds <philipstudentemail@gmail.com>
2021-02-27 10:14:05 -0600

    In this commit I updated the app to repond differently based on the website

--- popup.js
+++ popup.js
@@ -1,5 +1,13 @@
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     let Adreess = tabs[0].url;
     document.getElementById("submit").innerHTML = Adreess;
+    var link = String(tabs[0].url);
+    document.getElementById("test").innerHTML = link;
+    if (link=="https://www.youtube.com/"){
+        document.getElementById("test2").innerHTML = link;
+    }
+    else{
+        document.getElementById("test2").innerHTML = "nope";
+    }
     
 });
