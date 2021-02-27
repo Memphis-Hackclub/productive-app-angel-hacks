811a22cacfb7fdd22ef805fc035c42cd5b137674
Philip Pounds <philipstudentemail@gmail.com>
2021-02-27 16:00:17 -0600

    tiner

--- popup.js
+++ popup.js
@@ -2,6 +2,8 @@
 localStorage.website0 = website0;
 document.getElementById("test3").innerHTML = localStorage.website0;*/
 
+
+
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     let Adreess = tabs[0].url;
     
@@ -12,7 +14,22 @@ chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
         document.getElementById("content").innerHTML = '<h1>Add Website Monitors</h1><form>How goal crushing is this website?<input type="link" id="website"></input><select id="type" name="type"><option value="1">I will be fine</option><option value="2">Prabably an issue</option><option value="3">I will die looking at this website</option></select><br></form>';
     }
     if (link=="https://www.youtube.com/"){
-        document.getElementById("content").innerHTML = link;
+        var date = new Date();
+        var oldtime = date.getTime();
+        var myVar = setInterval(myTimer, 1000);
+        document.getElementById("content").innerHTML = myVar;
+        var timespent = myVar - oldtime;
+        document.getElementById("test").innerHTML = timespent;
     }
 
 });
+
+
+function myTimer() {
+    var date = new Date();
+    var newtime = date.getTime();
+    newtime = newtime/1000;
+    document.getElementById("test3").innerHTML = newtime;
+    return newtime
+
+    }
